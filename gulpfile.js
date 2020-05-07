9e1d6d101c51dd7529286d4870e3bea0a141dd15
nayac <naime0119@gmail.com>
2020-05-07 05:52:40 +0300

    second commit with the showcase done

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,11 @@ const browserSync = require('browser-sync').create();
 // compile scss into css
 function  style(){
     // 1.where is my scss file
-    return gulp.src('./src/scss/**/*.scss')
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','./src/scss/*.scss'])
         // 2.pass that file through sass compiler
         .pipe(sass().on('error', sass.logError))
          // 3.where do i save the compiled css?
-        .pipe(gulp.dest('./css'))
+        .pipe(gulp.dest('./src/css'))
 
         // 4.trem changes to all browser
         .pipe(browserSync.stream());
@@ -20,13 +20,39 @@ function  style(){
 function watch(){
     browserSync.init({
         server:{
-            baseDir: './'
+            baseDir: './src'
         }
     });
     gulp.watch('./src/scss/**/*.scss', style);
-    gulp.watch('./*.html').on('change', browserSync.reload);
-    gulp.watch('./js/**/*.js').on('change', browserSync.reload)
+    gulp.watch('node_modules/font-awesome/fonts/*', fonts);
+    gulp.watch('node_modules/font-awesome/css/font-awesome.min.css', fa);
+    gulp.watch('./src/*.html').on('change', browserSync.reload);
+    gulp.watch('./src/js/**/*.js').on('change', browserSync.reload);
 }
+
+
+//  move the fonts folder to src/fonts
+function  fonts(){
+    return gulp.src('node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest("src/fonts"));
+}
+
+//  move the font awesome css to src/css
+function  fa(){
+    return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+    .pipe(gulp.dest("src/css"));
+}
+// move js files to src/js
+function js(){
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+    .pipe(gulp.dest("src/js"))
+    .pipe(browserSync.stream());
+}
+
+
 
 exports.style = style;
 exports.watch = watch;
+exports.fonts = fonts;
+exports.fa = fa;
+exports.js = js;
